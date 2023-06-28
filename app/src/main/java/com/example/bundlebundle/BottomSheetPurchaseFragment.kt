ba5d994330af9d1a4edd59c97b8a104c6afb7578
sieon <noino0819@naver.com>
2023-06-28 18:09:02 +0900

    feat: product controller

--- app/src/main/java/com/example/bundlebundle/BottomSheetPurchaseFragment.kt
+++ app/src/main/java/com/example/bundlebundle/BottomSheetPurchaseFragment.kt
@@ -13,6 +13,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 class BottomSheetPurchaseFragment : BottomSheetDialogFragment() {
     private lateinit var tvQuantity: TextView
     private var quantity = 0
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_bottom_sheet_purchase, container, false)
         return view
@@ -21,17 +22,31 @@ class BottomSheetPurchaseFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tvQuantity = view.findViewById(R.id.tvQuantity)
+
         val btnMinus = view.findViewById<Button>(R.id.btnMinus)
         btnMinus.setOnClickListener {
             decrementQuantity()
         }
+
+        val btnPlus = view.findViewById<Button>(R.id.btnPlus)
+        btnPlus.setOnClickListener {
+            incrementQuantity()
+        }
+
         val bottomSheetBehavior = BottomSheetBehavior.from(view.parent as View)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
     }
+
     private fun decrementQuantity() {
         if (quantity > 0) {
             quantity--
             tvQuantity.text = quantity.toString()
         }
     }
+
+    private fun incrementQuantity() {
+        quantity++
+        tvQuantity.text = quantity.toString()
+    }
 }
+
